fef0fdc863a5bb02a8fe78384f8479991c42c89d
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-18 17:55:21 -0700

    Student Creation Form Challenge Complete

--- hackbright-web.py
+++ hackbright-web.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = request.args.get('github', 'jhacks')
+    github = request.args.get('github', 'jhacks') # Always says jhacks
     first, last, github = hackbright.get_student_by_github(github)
     html = render_template("student_info.html",
                             first=first,
@@ -18,6 +18,7 @@ def get_student():
 
     return html
 
+
 @app.route("/student_search")
 def get_student_form():
     """Show form for searching for a student."""
@@ -25,7 +26,33 @@ def get_student_form():
     return render_template("student_search.html")
 
 
+@app.route("/student_creator")
+def student_creator():
+    """Show form for creating a student."""
+
+    return render_template("student_creator.html")
+
+
+@app.route("/student_created", methods=['POST'])
+def student_created():
+    """Show results from student_creator."""
+
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+    github = request.form.get("github")
+
+    hackbright.make_new_student(fname, lname, github)
+
+    return render_template("student_created.html",
+                            fname=fname,
+                            lname=lname,
+                            github=github)
+
     
+
+
+
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
